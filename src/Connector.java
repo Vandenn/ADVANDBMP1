1230088f0c37b2f8358efb43868a943169cd89ca
Vandenn <evan.livelo@gmail.com>
2014-10-31 17:55:37 +0800

    Initial files for the ADVANDB MP1.

---
+++ src/Connector.java
@@ -0,0 +1,15 @@
+
+/**
+ * This is the class that will connect to the SQL Database.
+ */
+public class Connector {
+    
+    /**
+     * Constructor.
+     */
+    public Connector()
+    {
+        System.out.println("Insert connection code here.");
+    }
+    
+}
---
+++ src/MainClass.java
@@ -0,0 +1,16 @@
+/**
+ * ADVANDB MP1
+ * Chua, Livelo, Ver, Yao
+ */
+
+/**
+ * This is the class that holds the main function of the program.
+ */
+public class MainClass {
+    
+    public static void main(String args[])
+    {
+        System.out.println("ADVANDB MP1");
+    }
+    
+}

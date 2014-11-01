a91746af45ebf00ba89a1edf0316443ce50c2b89
Vandenn <evan.livelo@gmail.com>
2014-11-01 11:53:25 +0800

    Formatted SQLEnum

--- src/SQLEnum.java
+++ src/SQLEnum.java
@@ -2,5 +2,19 @@
  * Enum to contain SQL Queries for faster accessing.
  */
 public enum SQLEnum {
-
+    ORIGINAL, OPTIMIZED;
+    
+    @Override
+    public String toString()
+    {
+        switch(this)
+        {
+            case ORIGINAL:
+                return "SELECT * FROM employees";
+            case OPTIMIZED:
+                return "SELECT * FROM employees";
+            default:
+                return "SELECT * FROM employees";
+        }
+    }
 }
